4b2e53f09ef9e3b8bc436476b1301d9a8d34eecf
Michael Henry <mike@digitaljavelina.com>
2019-01-10 21:25:38 -0700

    added simple About page

---
+++ src/pages/about.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About Me</h1>
+      <p>
+        I thought today we would do a happy little picture. Go out on a limb -
+        that's where the fruit is. Now let's put some happy little clouds in
+        here. You're meant to have fun in life. Just use the old one inch brush.
+        There's nothing wrong with having a tree as a friend.
+      </p>
+
+      <p>
+        I guess I'm a little weird. I like to talk to trees and animals. That's
+        okay though; I have more fun than most people. The very fact that you're
+        aware of suffering is enough reason to be overjoyed that you're alive
+        and can experience it. It just happens - whether or not you worried
+        about it or tried to plan it. Let's get wild today. Everybody needs a
+        friend. Everything's not great in life, but we can still find beauty in
+        it.
+      </p>
+    </div>
+  </Layout>
+);
+
+export default About;
